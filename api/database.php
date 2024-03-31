35e5387edd2e8a5e57854bf03d0202bac5b4d2c0
Nimrod León <nleonc14@gmail.com>
2024-03-30 19:46:59 -0500

    eloquent config

---
+++ api/database.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'localhost',
+            'username' => 'root',
+            'password' => 'sysadmin',
+        ],
+    ],
+];
--- api/index.php
+++ api/index.php
@@ -4,7 +4,11 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
+$capsule = new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection(require __DIR__ . "/database.php");
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 $app = AppFactory::create();
 
