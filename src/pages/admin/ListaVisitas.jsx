d077b75ad84d1df20ef5d1ce4ca997838cfacf87
Nimrod León <nleonc14@gmail.com>
2024-04-07 15:33:39 -0500

    fix: html registrar visita

--- src/pages/admin/ListaVisitas.jsx
+++ src/pages/admin/ListaVisitas.jsx
@@ -7,6 +7,9 @@ import { Column } from "primereact/column"
 import { Paginator } from "primereact/paginator"
 import { Button } from "primereact/button"
 import { RegistrarVisitaModal } from "./RegistrarVisitaModal"
+import { InputTextarea } from "primereact/inputtextarea"
+import { Panel } from "primereact/panel"
+import { AutoComplete } from "primereact/autocomplete"
 
 export const ListaVisitas = () => {
     const [dates, setDates] = useState(null)
@@ -27,18 +30,82 @@ export const ListaVisitas = () => {
     return (
         <Fragment>
             <Header />
-            <main className="mx-2">
-                <h3 className="my-1">REGISTRO DE VISITAS</h3>
-                <div className="flex gap-3 mb-3">
+            <Panel header="Registrar Visita" toggleable className="m-2">
+                <div className="grid gap-3 mb-3">
+                    <div className="col">
+                        <div className="p-inputgroup flex-1">
+                            <span className="p-float-label">
+                                <InputText id="username" style={{ width: '100%' }} />
+                                <label htmlFor="username">D.N.I</label>
+                            </span>
+                            <Button icon="pi pi-search" severity="success" className="px-4" />
+                        </div>
+                    </div>
+                    <div className="col">
+                        <span className="p-float-label">
+                            <InputText id="username" style={{ width: '100%' }} />
+                            <label htmlFor="username">Nombres y Apellidos</label>
+                        </span>
+                    </div>
+                    <div className="col">
+                        <div className="flex gap-2">
+                            <span className="p-float-label" style={{ width: '100%' }}>
+                                <AutoComplete id="username" dropdown={true}
+                                    style={{ width: '100%' }} inputStyle={{ width: '100%' }} />
+                                <label htmlFor="username">Entidad del Visitante</label>
+                            </span>
+                            <Button icon="pi pi-plus" className="p-button-secondary" />
+                        </div>
+                    </div>
+                </div>
+                <div className="grid gap-3 mb-3">
+                    <div className="col">
+                        <div className="flex gap-2">
+                            <span className="p-float-label" style={{ width: '100%' }}>
+                                <AutoComplete id="username" dropdown={true}
+                                    style={{ width: '100%' }} inputStyle={{ width: '100%' }} />
+                                <label htmlFor="username">Lugar</label>
+                            </span>
+                            <Button icon="pi pi-plus" className="p-button-secondary" />
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="flex gap-2">
+                            <span className="p-float-label" style={{ width: '100%' }}>
+                                <AutoComplete id="username" dropdown={true}
+                                    style={{ width: '100%' }} inputStyle={{ width: '100%' }} />
+                                <label htmlFor="username">Funcionario</label>
+                            </span>
+                            <Button icon="pi pi-plus" className="p-button-secondary" />
+                        </div>
+                    </div>
+                    <div className="col">
+                        <span className="p-float-label">
+                            <InputText id="username" style={{ width: '100%' }} />
+                            <label htmlFor="username">Motivo</label>
+                        </span>
+                    </div>
+                </div>
+                <span className="p-float-label mb-3">
+                    <InputTextarea autoResize style={{ width: '100%' }} />
+                    <label htmlFor="username">Observación</label>
+                </span>
+                <div className="flex justify-content-center gap-3">
+                    <Button label="Limpiar" icon="pi pi-eraser" severity="secondary" />
+                    <Button label="Guardar" icon="pi pi-save" severity="success" />
+                </div>
+            </Panel>
+            <Panel header="Registro de Visitas" className="mx-2">
+                <h3 className="text-center">Opciones de Busqueda</h3>
+                <div className="flex justify-content-center gap-3 mb-3">
                     <Calendar value={dates} onChange={(e) => setDates(e.value)} selectionMode="range" showIcon />
                     <span className="p-input-icon-left">
                         <i className="pi pi-search" />
                         <InputText placeholder="Buscar" />
                     </span>
-                    <Button onClick={handleRegistrarVisita} icon="pi pi-plus" severity="info" label="Registrar Visita" />
                 </div>
                 <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="fecha_registro" header="Fecha de Registro"></Column>
+                    <Column field="fecha_registro" header="Fecha de Registro"></Column>
                     <Column field="fecha_visita" header="Fecha de Visita"></Column>
                     <Column field="entidad_visitada" header="Entidad Visitada"></Column>
                     <Column field="visitante" header="Visitante"></Column>
@@ -52,7 +119,7 @@ export const ListaVisitas = () => {
                     <Column field="observación" header="Observación"></Column>
                 </DataTable>
                 <Paginator first={first} rows={rows} totalRecords={120} rowsPerPageOptions={[10, 20, 30]} onPageChange={onPageChange} />
-            </main>
+            </Panel>
             <RegistrarVisitaModal
                 visible={visible}
                 setVisible={setVisible}
