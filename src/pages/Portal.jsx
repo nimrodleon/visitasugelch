f85311ecf57e424e68b473c744a4701688451b6e
Nimrod León <nleonc14@gmail.com>
2024-04-11 00:23:15 -0500

    boton excel

--- src/pages/Portal.jsx
+++ src/pages/Portal.jsx
@@ -10,6 +10,7 @@ import { Panel } from "primereact/panel"
 import { getAsistencias } from "../api"
 import { useFormik } from "formik"
 import * as Yup from "yup"
+import { Button } from "primereact/button"
 
 export const Portal = () => {
     const navigate = useNavigate()
@@ -122,6 +123,7 @@ export const Portal = () => {
                             onKeyDown={(e) => e.key === 'Enter' && queryForm.handleSubmit()}
                             placeholder="Buscar" />
                     </span>
+                    <Button label="Excel" severity="success" />
                 </div>
                 <DataTable value={asistencias.data} tableStyle={{ minWidth: '50rem' }}>
                     <Column field="fecha_registro" header="Fecha de Registro"></Column>
